6bcd8c07d41aa730fcbaee467c655970a93da1d4
Maxwell Zhou <yale.developer@gmail.com>
2014-07-17 11:19:36 -0700

    first commit

---
+++ BNRCoursesViewController.h
@@ -0,0 +1,17 @@
+//
+//  BNRCoursesViewController.h
+//  Nerdfeed
+//
+//  Created by Maxwell Zhou on 7/17/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BNRWebViewController;
+
+@interface BNRCoursesViewController : UITableViewController
+
+@property (nonatomic, strong) BNRWebViewController *webViewController;
+
+@end
